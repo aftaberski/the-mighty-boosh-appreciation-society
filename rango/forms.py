5fa10e3b6fbc3ae9da620fdb026baf33322625e0
Anna Taberski <aftaberski@gmail.com>
2015-05-02 11:22:47 -0400

    Add view for page form

--- rango/forms.py
+++ rango/forms.py
@@ -17,6 +17,22 @@ class PageForm(forms.ModelForm):
 	url = forms.URLField(max_length=200, help_text="Enter the URL")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+	# Overriding methods implemented as part of the
+	# Django framework can provide you with an
+	# elegant way to add that extra bit of functionality
+
 	class Meta:
 		model = Page
 		exclude = ('category',)
+
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+
+    # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+
+		return cleaned_data
+
--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -57,3 +57,30 @@ def add_category(request):
 	# Bad form (or form details)...
 	# Render the form with error messages (if any)
 	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+
+				# potentially better to use a redirect here
+				return category(request, category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+
+	context_dict = {'form': form, 'category': cat}
+
+	return render(request, 'rango/add_page', context_dict)
