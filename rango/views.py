d6591d9aca4c6de13ba68e0e67af8c26254b93fe
aftaberski <aftaberski@gmail.com>
2015-05-07 15:35:02 -0400

    Add counter that tracks number of visits

--- rango/views.py
+++ rango/views.py
@@ -3,14 +3,46 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 
+from datetime import datetime
+
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
 def index(request):
-	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories' : category_list}
-	return render(request, 'rango/index.html', context_dict)
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {'categories': category_list, 'pages': page_list}
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 
 def about(request):
 	return render(request, 'rango/about.html')
@@ -92,7 +124,6 @@ def add_page(request, category_name_slug):
 	return render(request, 'rango/add_page.html', context_dict)
 
 def register(request):
-
 	# Was the registration successful?
 	registered = False
 
@@ -162,7 +193,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-	return HttpResponse("Since you're logged in your can see this!")
+	return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
